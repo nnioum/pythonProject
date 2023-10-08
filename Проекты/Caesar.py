a30f3355daf03a2bd39b06475c6fd5b44158be3d
Sima <simapono.4@gmail.com>
2023-10-08 16:38:04 +0700

    Initial commit

---
+++ Проекты/Caesar.py
@@ -0,0 +1,88 @@
+def english():
+    b = ''
+    a = input('Введите тест - ')
+    n = int(input('Какой сдвиг?'))
+    for i in a:
+        k = int(ord(i))
+        if 97 <= k <= 122:
+            i = chr(k+n)
+            if k+n > 122:
+                i = chr((k+n - 122) + 96)
+        b += i
+    return print(b)
+def russian():
+    b = ''
+    a = input('Введите тест - ')
+    n = int(input('Какой сдвиг?'))
+    for i in a:
+        k = int(ord(i))
+        if (160 <= k <= 175) or (224 <= k <= 239):
+            i = chr(k+n)
+            if k+n > 175:
+                i = chr((k - 175) + 223)
+            elif k+n > 239:
+                i = chr((k+n - 239) + 159)
+        b += i
+    return print(b)
+
+def dec():
+    lang = input('Какой язык? - ')
+    b = input('Введите тест - ')
+    a = int(input('Какой сдвиг?'))
+    if lang == 'русский':
+        dict = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    else:
+        dict = 'abcdefghijklmnopqrstuvwxyz'
+    word = ''
+    for i in b:
+        if i in dict:
+            x = dict.find(i)
+            num = x - a
+            word += dict[num]
+        else:
+            word += i
+    return print(word)
+
+
+def encr():
+    lang = input('Какой язык? - ')
+    if lang == 'русский':
+        russian()
+    else:
+        english()
+
+what = input('Что делать? - ')
+if what == 'расшифровать':
+    dec()
+else:
+    encr()
+
+# # На вход программе подается строка текста на английском языке, в которой нужно зашифровать все слова.
+# # Каждое слово строки следует зашифровать с помощью шифра Цезаря (циклического сдвига на длину этого слова).
+# # Строчные буквы при этом остаются строчными, а прописные – прописными.
+# a = input()
+# b = ''
+# for i in a:
+#     if i in ' abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM':
+#         b += i
+# b = b.split()
+# a = a.split()
+# k = ''
+# d = -1
+# for i in a:
+#     n = len(b[d+1])
+#     for j in i:
+#         s = ord(j)
+#         if 97 <= s <= 122:
+#             j = chr(s+n)
+#             if s+n > 122:
+#                 j = chr((s+n-122)+96)
+#         elif 65 <= s <= 90:
+#             j = chr(s+n)
+#             if s+n > 90:
+#                 j = chr(s+n-90+64)
+#
+#         k += j
+#     k += ' '
+#     d += 1
+# print(k)
---
+++ Проекты/mini--project3.py
@@ -0,0 +1,53 @@
+import random
+def check_of(a):
+    pos_answer = ['да', 'конечно', 'yes', '1']
+    nog_answer = ['не надо', 'no', 'нет', '0']
+    n = input().lower()
+    while True:
+        if n in pos_answer:
+            return 'да'
+            break
+        elif n in nog_answer:
+            return 'нет'
+            break
+        else:
+            print('Я вас не понимаю, попробуйте по другому')
+            n = input().lower()
+print('Добро пожаловать в "Генератор безопасных паролей".')
+print('Эта программа создана специально, чтобы помочь вам создать безопасный пороль.')
+print('Давайте начнем!')
+quantity = int(input('Какое количество паролей для генерации вы хотите? - '))
+len_wassword = int(input('Какую длину пароля вы хотите? - '))
+num = check_of(print('Включать ли цифры 0123456789?'))
+upper = check_of(print('Включать ли заглавные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?'))
+lower = check_of(print('Включать ли прописные буквы abcdefghijklmnopqrstuvwxyz?'))
+symbol = check_of(print('Включать ли символы !#$%&*+-=?@^_?'))
+amb_char = check_of(print('Исключать ли неоднозначные символы il1Lo0O?'))
+digits = '23456789'
+lowercase_letters = 'abcdefghjkmnpqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKMNPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=@^_.'
+
+for i in range(quantity):
+    char = ''
+    while True:
+        if num == 'да':
+            char += random.choice(digits)
+            if len(char) == len_wassword:
+                break
+        if upper == 'да':
+            char += random.choice(uppercase_letters)
+            if len(char) == len_wassword:
+                break
+        if lower == 'да':
+            char += random.choice(lowercase_letters)
+            if len(char) == len_wassword:
+                break
+        if amb_char == 'нет':
+            char += random.choice('il1Lo0O')
+            char += random.choice(lowercase_letters)
+            if len(char) == len_wassword:
+                break
+    print(char)
+print('Спасибо, что пользовались нашими услугами.')
+print('До новых встреч!!!')
